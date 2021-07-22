9604cb4065042a155338eaab7236a00c786bbe1c
yavv951 <vadimv95189@gmail.com>
2021-07-22 14:27:55 +0300

    update file main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 with open("moon.txt", "r") as fd:
     pathArr = fd.read()
     expected = calculate(pathArr)
-def calculate(pathArr):
+
+#Функция, которая считает количество кратеров.
+def calculate(pathArr: list) -> int:
     expected = 0
     for x in pathArr:
         for y in x:
@@ -9,28 +11,28 @@ def calculate(pathArr):
                 expected += 1
                 return main()
     return expected
-def main(pathArr):
+#Функция, которая обрабатывает лист на кратеры.
+def main(pathArr: list) -> int:
     weight = 1
     n = 0
     for i in range(len(pathArr) * len(pathArr[0])):
         for y in range(len(pathArr)):
             for x in range(len(pathArr[y])):
                 if pathArr[y][x] == weight:
-                # Вниз
-                if y > 0 and pathArr[y - 1][x] == 0:
-                    pathArr[y - 1][x] = weight + 1
-                # Вверх
-                if y < (len(pathArr) - 1) and pathArr[y + 1][x] == 0:
-                    pathArr[y + 1][x] = weight + 1
-                # Вправо
-                if x > 0 and pathArr[y][x - 1] == 0:
-                    pathArr[y][x - 1] = weight + 1
-                # Влево
-                if x < (len(pathArr[y]) - 1) and pathArr[y][x + 1] == 0:
-                    pathArr[y][x + 1] = weight + 1
-                    n += 1
+                    # Вниз
+                    if y > 0 and pathArr[y - 1][x] == 0:
+                        pathArr[y - 1][x] = weight + 1
+                    # Вверх
+                    if y < (len(pathArr) - 1) and pathArr[y + 1][x] == 0:
+                        pathArr[y + 1][x] = weight + 1
+                    # Вправо
+                    if x > 0 and pathArr[y][x - 1] == 0:
+                        pathArr[y][x - 1] = weight + 1
+                    # Влево
+                    if x < (len(pathArr[y]) - 1) and pathArr[y][x + 1] == 0:
+                        pathArr[y][x + 1] = weight + 1
+                        n += 1
                     return weight / n
     return
 print(calculate(pathArr))
-
 
